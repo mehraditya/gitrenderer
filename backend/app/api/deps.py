bd7f6d6598901b7442bd3d560966e338d57dc141
adityamehracr7@gmail.com <adityamehracr7@gmail.com>
2026-02-03 00:10:21 +0530

    Added SessionLocal from db

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,4 +1,13 @@
 from app.services.storage import LocalStorage 
+from app.core.db import SessionLocal 
+
 
 def get_Storage()-> LocalStorage:
     return LocalStorage()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
